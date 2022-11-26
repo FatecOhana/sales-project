b4cdef6a4d0a47f4822ca293d5acb1c502fc4c01
Guilherme Palma <guippalma@gmail.com>
2022-11-25 22:14:07 -0300

    refact: change class names

--- database/configuration/DatabaseConfiguration.php
+++ database/configuration/DatabaseConfiguration.php
@@ -1,8 +1,8 @@
 <?php
 
-class HandlerDatabase
+class DatabaseConfiguration
 {
-    public $connection;
+    public PDO $connection;
 
     /**
      * Open connection in database
@@ -11,13 +11,10 @@ class HandlerDatabase
     public function OpenConnection()
     {
         try {
-            // Configura a Conexão e Habilita a Obtenção de Erros
             $connection = new PDO("mysql: host=localhost;dbname=sales_project", "admin", "admin");
             $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-            // Atribui à Variavel da Classe a Conexão
             $this->connection = $connection;
-
         } catch (PDOException $ex) {
             $connection = null;
             throw new Exception("Erro na Conexão com o Banco de Dados." .
